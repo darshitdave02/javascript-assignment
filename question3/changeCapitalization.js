20ab365036b3f2b9e3c7972586566a49e763b8bf
Darshit Dave <darshitdave02@gmail.com>
2023-01-16 02:32:07 +0530

    feat: change capitalization of a string

---
+++ question3/changeCapitalization.js
@@ -0,0 +1,42 @@
+function changeCapitalization1 ( sentence ) {
+    
+    let string = ""
+
+    for(let i=0; i<sentence.length; i++)
+    {
+        if(sentence[i] === sentence[i].toLowerCase())
+        {
+            string+=sentence[i].toUpperCase()
+        }
+        else
+        {
+            string+=sentence[i].toLowerCase()
+        }
+    }
+
+    return string;
+
+}
+
+const changeCapitalization2 = ( sentence ) => {
+
+
+    let string = ""
+
+    for(let i=0; i<sentence.length; i++)
+    {
+        if(sentence[i] === sentence[i].toLowerCase())
+        {
+            string+=sentence[i].toUpperCase()
+        }
+        else
+        {
+            string+=sentence[i].toLowerCase()
+        }
+    }
+
+    return string;
+}
+
+module.exports = changeCapitalization1;
+module.exports = changeCapitalization2;
