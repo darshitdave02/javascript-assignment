365f51e1532ad63cd73556e13e32fb59dc7575a8
Darshit Dave <darshitdave02@gmail.com>
2023-01-16 02:38:29 +0530

    feat: functionality to concatenate strings after removing first character

---
+++ question7/concatenate.js
@@ -0,0 +1,27 @@
+function concatenate1 (stringArray) {
+
+    let ans = "";
+
+    for(let i=0; i<stringArray.length; i++)
+    {
+        stringArray[i] = stringArray[i].slice(1);
+        ans += stringArray[i];
+    }
+
+    return ans;
+}
+
+const concatenate2 = (stringArray) => {
+    let ans = "";
+
+    for(let i=0; i<stringArray.length; i++)
+    {
+        stringArray[i] = stringArray[i].slice(1);
+        ans += stringArray[i];
+    }
+
+    return ans;
+}
+
+module.exports = concatenate1;
+module.exports = concatenate2;
