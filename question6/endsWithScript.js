601e9719b82d2a2f49d9007c94c0f9fe31d4b6ec
Darshit Dave <darshitdave02@gmail.com>
2023-01-16 02:37:01 +0530

    feat: functionality to check if string ends with Script

---
+++ question6/endsWithScript.js
@@ -0,0 +1,13 @@
+function endsWithScript1 (string) {
+
+    return string.endsWith('Script');
+}
+
+const endsWithScript2 = (string) => {
+
+    return string.endsWith('Script');
+
+}
+
+module.exports = endsWithScript1;
+module.exports = endsWithScript2;
