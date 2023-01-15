b5e51c172382b0d296bab5ca799eb8630d2b56a5
Darshit Dave <darshitdave02@gmail.com>
2023-01-16 02:41:54 +0530

    feat: replace every character in a given string with the character following it in the alphabet

---
+++ question9/replaceWithNextCharacterInString.js
@@ -0,0 +1,49 @@
+function replaceWithNextCharacterInString1 (string) {
+
+    let ans = "";
+
+    for(let i=0; i<string.length; i++)
+    {
+            switch(string[i]) {
+                
+                case 'z':
+                ans += 'a';
+                break;
+                case 'Z':    
+                ans += 'A';
+                break;
+                default:
+                    let value = string.charAt(i).charCodeAt()
+                    value = value + 1
+                    ans  += String.fromCharCode(value)
+                    
+                }
+    }
+    return ans;
+}
+
+const replaceWithNextCharacterInString2 = (string) => {
+    let ans = "";
+
+    for(let i=0; i<string.length; i++)
+    {
+            switch(string[i]) {
+                
+                case 'z':
+                ans += 'a';
+                break;
+                case 'Z':    
+                ans += 'A';
+                break;
+                default:
+                    let value = string.charAt(i).charCodeAt()
+                    value = value + 1
+                    ans  += String.fromCharCode(value)
+                    
+                }
+    }
+    return ans;
+}
+
+module.exports = replaceWithNextCharacterInString1;
+module.exports = replaceWithNextCharacterInString2;
