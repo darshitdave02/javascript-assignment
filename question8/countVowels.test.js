05340146d2d78b440b7472dcd3486fc87c422c8c
Darshit Dave <darshitdave02@gmail.com>
2023-01-16 02:40:16 +0530

    chore: test file for counting vowels in a string

---
+++ question8/countVowels.test.js
@@ -0,0 +1,25 @@
+const countVowels1 = require('./countVowels')
+const countVowels2 = require('./countVowels')
+
+describe('Test to count number of Vowels',()=>{
+    test('test case one',()=>{
+        
+        expect(countVowels1("Hello")).toBe(2)
+    })
+
+    test('test case two',()=>{
+        expect(countVowels2("Moto")).toBe(2)
+        
+    })
+
+    test('test case three',()=>{
+       
+        expect(countVowels2("Umbrella")).toBe(3)
+    })
+
+    test('test case four',()=>{
+       
+        expect(countVowels1("Icecream")).toBe(4)
+    })
+}
+)
