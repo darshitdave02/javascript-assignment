241fe81818957bb1599eef0842c666cfcea5eae3
Darshit Dave <darshitdave02@gmail.com>
2023-01-16 02:38:58 +0530

    chore: test files for checking if strings are concatenated after removing first character

---
+++ question7/concatenate.test.js
@@ -0,0 +1,22 @@
+const concatenate1 = require('./concatenate')
+const concatenate2 = require('./concatenate')
+
+describe('Test to check concatenation of strings without first character',()=>{
+    test('test case one',()=>{
+        
+        expect(concatenate1(["hello","guys"])).toBe("ellouys")
+    })
+
+    test('test case two',()=>{
+        expect(concatenate2(["Code","Academy"])).toBe("odecademy")
+        
+    })
+
+    test('test case three',()=>{
+       
+        expect(concatenate1(["One","Two"])).toBe("newo")
+    })
+
+   
+}
+)
