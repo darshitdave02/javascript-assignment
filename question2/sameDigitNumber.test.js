577ed01bca5691e14338d52512c0b9cb6395eae3
Darshit Dave <darshitdave02@gmail.com>
2023-01-16 02:31:15 +0530

    chore: test file to check if number is consist of same digits

---
+++ question2/sameDigitNumber.test.js
@@ -0,0 +1,28 @@
+const findSameDigitNumber1 = require('./sameDigitNumber')
+const findSameDigitNumber2 = require('./sameDigitNumber')
+
+describe('Test to check same digit number',()=>{
+    
+    test('2222 is a same digit number',() => {
+        
+        expect(findSameDigitNumber1(2222)).toBeTruthy();
+    })
+    
+    test('2345 is not a same digit number',() => {
+        
+        expect(findSameDigitNumber2(2345)).toBeFalsy();
+    })
+
+    test('99999 is a same digit number',() => {
+        
+        expect(findSameDigitNumber2(99999)).toBeTruthy();
+    })
+
+    test('3334 is not a same digit number',() => {
+        
+        expect(findSameDigitNumber1(3334)).toBeFalsy();
+    })
+    
+})
+
+
