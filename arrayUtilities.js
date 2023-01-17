6ca7720d08fb0fa6c9a45c3186a0f2a3112293c6
Darshit Dave <darshitdave02@gmail.com>
2023-01-17 09:59:17 +0530

    feat: replace map and filter with reduce in array operation function

--- arrayUtilities.js
+++ arrayUtilities.js
@@ -45,9 +45,34 @@ const arrayOperation = (numbers) => {
   });
 };
 
+const arrayOperation2 = (numbers) => {
+
+  if (!Array.isArray(numbers)) {
+    throw new Error('Input is not an array');
+  }
+      
+  if(!numbers.every(element =>  typeof element === 'number')){
+    throw new Error('Array should consist of numbers only');
+  }
+      
+  return numbers.reduce((acc, curr) => {
+    if(curr%2===0)
+    {
+      acc.push(curr);
+    }
+    return acc;
+  },[]);
+    
+};
+  
+
+
+
+
+
 console.log(arrayOperation([1,2,3]));
 
 
 
 
-module.exports = { doubleNumbers, filterEvenNumbers, arrayOperation };
+module.exports = { doubleNumbers, filterEvenNumbers, arrayOperation, arrayOperation2 };
